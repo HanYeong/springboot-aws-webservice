1e254609458ffc63048ace384e1cede8c3e69085
HanyeongJi <sav113@mcocoa.com>
2022-10-25 18:04:23 +0900

    lombok 연동 및 테스트

--- src/test/java/com/springaws/study/web/HelloControllerTest.java
+++ src/test/java/com/springaws/study/web/HelloControllerTest.java
@@ -1,17 +1,15 @@
 package com.springaws.study.web;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class) // JUnit 실행자 외 다른 실행자와 함께 실행. 스프링부트 테스트와 연동
 @WebMvcTest(controllers = HelloController.class) // Web(Spring MVC) 테스트
@@ -27,5 +25,19 @@ public class HelloControllerTest {
 		mvc.perform(get("/hello")) // GET 요청
 				.andExpect(status().isOk()) // 결과 검증 http header code : 200
 				.andExpect(content().string(hello)); // 결과 검증
+	}
+
+	@Test
+	public void helloDtoTest() throws Exception {
+		String name = "hello";
+		int amount = 1000;
+
+		mvc.perform(
+				get("/hello/dto")
+						.param("name", name)
+						.param("amount", String.valueOf(amount)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name", is(name)))
+				.andExpect(jsonPath("$.amount", is(amount)));
 	}
 }
